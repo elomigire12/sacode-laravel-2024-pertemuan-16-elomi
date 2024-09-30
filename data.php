dd1a76788ee7e8d49af8bf8b9f987ceaa322e785
Elomi Gire <elomi.gire.78@gmail.com>
2024-09-30 20:12:27 +0900

    tambah data.php dan detail.php

---
+++ data.php
@@ -0,0 +1,42 @@
+
+
+<?php
+
+$mahasiswa = [
+
+    [
+        'id' => 1,
+        'nim' => "16411045",
+        'nama' => "Samuel Bosawer",
+        'alamat' => "Sentani",
+        'jurusan' => "Teknik Informatika"
+    ],
+
+    [
+        'id' => 2,
+        'nim' => "16411046",
+        'nama' => "Elomi Gire",
+        'alamat' => "Waena",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+    [
+        'id' => 3,
+        'nim' => "16411021",
+        'nama' => "Agus Asso",
+        'alamat' => "Waena",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+    [
+        'id' => 4,
+        'nim' => "16411121",
+        'nama' => "Samuel Bosawer",
+        'alamat' => "Kotaraja",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+];
+
+
+?>
